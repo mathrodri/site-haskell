d7d71a2c54b6ae9f3802b3e138cb9a1822fdd672
Ubuntu <ubuntu@ip-172-31-85-242.ec2.internal>
2019-11-15 01:36:28 +0000

    fix_home

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -1,13 +1,18 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TypeFamilies #-}
 module Handler.Home where
 
 import Import
-import Network.HTTP.Types.Status
+import Text.Lucius
+import Text.Julius
+-- import Network.HTTP.Types.Status
 import Database.Persist.Postgresql
 
 getHomeR :: Handler Html
-getHomeR = undefined
+getHomeR = do
+    defaultLayout $ do 
+        $(whamletFile "templates/homepage.hamlet")
