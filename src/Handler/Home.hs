daeccc9b8825a23035d4b311f429f0be8f82effb
Ubuntu <ubuntu@ip-172-31-85-242.ec2.internal>
2019-11-28 19:23:34 +0000

    deploy

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -30,7 +30,7 @@ getHomeR = do
             <meta charset="UTF-8">
             <meta name="description" content="">
             <meta name="viewport" content="width=device-width, initial-scale=1">
-            <title>
+            <title>Site
         |]
         $(whamletFile "templates/homepage.hamlet")
     
