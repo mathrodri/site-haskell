de42d5f929a9a5199755450c4652304d9bd0806f
Ubuntu <ubuntu@ip-172-31-85-242.ec2.internal>
2019-11-28 19:22:43 +0000

    deploy

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -20,19 +20,17 @@ data FileForm = FileForm
 getHomeR :: Handler Html
 getHomeR = do
     defaultLayout $ do 
-        toWidgetHead $(luciusFile "templates/default-layout.lucius")
-        -- $(whamletFile "templates/aaa.hamlet")
-    --   addStylesheet (StaticR css_bootstrap_css) T6DQ0klE
-    --     addStylesheet (StaticR css_main_css)
-    --     addScript $ StaticR quiz_js
-    --     toWidgetHead $(luciusFile "templates/adCSS.lucius")
-    --     toWidgetHead $(juliusFile "templates/adScript.julius")
+        addStylesheet (StaticR css_bootstrap_css)
+        addStylesheet (StaticR css_main_css)
+        addScript $ StaticR quiz_js
+        toWidgetHead $(luciusFile "templates/adCSS.lucius")
+        toWidgetHead $(juliusFile "templates/adScript.julius")
         
-    --     [whamlet|
-    --         <meta charset="UTF-8">
-    --         <meta name="description" content="">
-    --         <meta name="viewport" content="width=device-width, initial-scale=1">
-    --         <title>
-    --     |]
+        [whamlet|
+            <meta charset="UTF-8">
+            <meta name="description" content="">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <title>
+        |]
         $(whamletFile "templates/homepage.hamlet")
     
