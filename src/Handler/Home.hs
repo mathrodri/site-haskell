0189fc522345c0118f7a8c19aee9c2404b971e97
Ubuntu <ubuntu@ip-172-31-85-242.ec2.internal>
2019-11-28 19:25:28 +0000

    deploy

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -22,7 +22,7 @@ getHomeR = do
     defaultLayout $ do 
         addStylesheet (StaticR css_bootstrap_css)
         addStylesheet (StaticR css_main_css)
-        addScript $ StaticR quiz_js
+        addScript (StaticR quiz_js)
         -- toWidgetHead $(luciusFile "templates/adCSS.lucius")
         -- toWidgetHead $(juliusFile "templates/adScript.julius")
         
