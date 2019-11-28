caaa179e0319175afb67aa1f8002ded7f73d69ee
Ubuntu <ubuntu@ip-172-31-85-242.ec2.internal>
2019-11-28 19:04:57 +0000

    deploy

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -20,4 +20,16 @@ data FileForm = FileForm
 getHomeR :: Handler Html
 getHomeR = do
     defaultLayout $ do 
+       addStylesheet (StaticR css_bootstrap_css)
+        addStylesheet (StaticR css_main_css)
+        addScript $ StaticR quiz_js
+        toWidgetHead $(luciusFile "templates/adCSS.lucius")
+        toWidgetHead $(juliusFile "templates/adScript.julius")
+        
+        [whamlet|
+            <meta charset="UTF-8">
+            <meta name="description" content="">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <title>
+        |]
         $(whamletFile "templates/homepage.hamlet")
