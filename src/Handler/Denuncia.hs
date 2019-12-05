41bde0f9630153996893df13bc0e78b947cbe12a
Ubuntu <ubuntu@ip-172-31-85-242.ec2.internal>
2019-12-05 14:10:59 +0000

    fix_form

--- src/Handler/Denuncia.hs
+++ src/Handler/Denuncia.hs
@@ -11,30 +11,31 @@ import Text.Lucius
 import Text.Julius
 -- import Network.HTTP.Types.Status
 import Database.Persist.Postgresql
+import Yesod.Form.Bootstrap
 
 data FileForm = FileForm
     { fileInfo :: FileInfo
         , fileDescription :: Text
     }
 
--- formDenuncia :: Form Denuncia 
--- formDenuncia = renderBootstrap $ Denuncia
---     <$> areq textField "Nome: " Nothing
---     <*> areq dayField "Email: " Nothing
---     <*> areq textField "Assunto: " Nothing
---     <*> areq textField "Mensagem: " Nothing
+formDenuncia :: Form Denuncia 
+formDenuncia = renderBootstrap $ Denuncia
+    <$> areq textField "Nome: " Nothing
+    <*> areq dayField "Email: " Nothing
+    <*> areq textField "Assunto: " Nothing
+    <*> areq textField "Mensagem: " Nothing
 
 postDenunciaR :: Handler Html
 postDenunciaR = do
-    -- ((result,_),_) <- runFormPost formDenuncia
-    -- case result of 
-    --     FormSuccess denuncia -> do 
-    --         runDB $ insert denuncia 
-    --         setMessage [shamlet|
-    --             <div>
-    --                 Denuncia feita com sucesso.
-    --         |]
-    --         redirect HomeR
-    --     _ -> redirect HomeR
-    redirect HomeR
+    ((result,_),_) <- runFormPost formDenuncia
+    case result of 
+        FormSuccess denuncia -> do 
+            runDB $ insert denuncia 
+            setMessage [shamlet|
+                <div>
+                    Denuncia feita com sucesso.
+            |]
+            redirect HomeR
+        _ -> redirect HomeR
+    -- redirect HomeR
     
