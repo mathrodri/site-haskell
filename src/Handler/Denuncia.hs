d31c7c394d8fbd30cbc0165482bd8c9fd73f53ad
Ubuntu <ubuntu@ip-172-31-85-242.ec2.internal>
2019-12-05 19:45:25 +0000

    fix_head_info

--- src/Handler/Denuncia.hs
+++ src/Handler/Denuncia.hs
@@ -11,31 +11,40 @@ import Text.Lucius
 import Text.Julius
 -- import Network.HTTP.Types.Status
 import Database.Persist.Postgresql
-import Yesod.Form
 
 data FileForm = FileForm
     { fileInfo :: FileInfo
         , fileDescription :: Text
     }
 
--- formDenuncia :: Form Denuncia 
--- formDenuncia = renderBootstrap3 BootstrapBasicForm $ Denuncia
---     <$> areq textField "Nome: " Nothing
---     <*> areq dayField "Email: " Nothing
---     <*> areq textField "Assunto: " Nothing
---     <*> areq textField "Mensagem: " Nothing
+formDenuncia :: Form (Denuncia, Text) 
+formDenuncia = renderBootstrap $ (,)
+    <$> (Denuncia 
+        <*> areq textField "Nome: " Nothing
+        <*> areq emailField "Email: " Nothing
+        <*> areq textField "Assunto: " Nothing
+        <*> areq textField "Mensagem: " Nothing)
 
+-- T6DQ0klE
+getDenunciaR :: Handler Html
+getDenunciaR = do 
+    (widget,_) <- generateFormPost formDenuncia
+    msg <- getMessage
+    defaultLayout $ do
+        setTitle "Coach no teu cool"
+        $(whamletFile "templates/denuncia.hamlet")
+        
 postDenunciaR :: Handler Html
 postDenunciaR = do
-    -- ((result,_),_) <- runFormPost formDenuncia
-    -- case result of 
-    --     FormSuccess denuncia -> do 
-    --         runDB $ insert denuncia 
-    --         setMessage [shamlet|
-    --             <div>
-    --                 Denuncia feita com sucesso.
-    --         |]
-    --         redirect HomeR
-    --     _ -> redirect HomeR
+    ((result,_),_) <- runFormPost formDenuncia
+    case result of 
+        FormSuccess denuncia -> do 
+            runDB $ insert denuncia 
+            setMessage [shamlet|
+                <div>
+                    Denuncia feita com sucesso.
+            |]
+            redirect HomeR
+        _ -> redirect HomeR
     redirect HomeR
     
